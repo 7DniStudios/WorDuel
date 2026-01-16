69afb2f7a9c696dea831a0d45f0a11f21998e2f9
Wojciech Orański <wojtekor9@gmail.com>
2026-01-16 18:03:36 +0100

    Add english words

--- Readme.md
+++ Readme.md
@@ -18,3 +18,6 @@ docker compose down -v
 
 
 Polish words were obtained from https://sjp.pl/sl/growy/ on 16.01.2026 and are used on the [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/) licence.
+
+English words were obtained from the [SOWPODS Scrabble Word List](https://www.wordgamedictionary.com/sowpods/),
+also on 16.01.2026.
---
+++ postgres/01_create_tables.sql
@@ -0,0 +1,22 @@
+
+
+\c worduel_db
+
+
+-- two different languages can have words that are spelled the same, so we use an autoincremented id
+-- we could use the word-language pair as primary key, but it doesn't feel like a good solution
+CREATE TABLE IF NOT EXISTS words(
+    word_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    word    char(8) NOT NULL,
+    lang    char(2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS word_stats(
+    word_id        INTEGER REFERENCES words(word_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    last_used      TIMESTAMP,
+    game_count     INTEGER NOT NULL DEFAULT 0 CHECK (game_count>=0),
+    won_game_count INTEGER NOT NULL DEFAULT 0 CHECK (won_game_count>=0),
+    guess_count    INTEGER NOT NULL DEFAULT 0 CHECK (guess_count>=0)
+);
+
+
