3efb4717eaba103e29d113832d0554b83eacae81
Bartlomiej Stefanski <bartex00001@gmail.com>
2026-01-17 17:41:32 +0100

    [#16] db: Add unique constraint for words table

--- postgres/01_create_tables.sql
+++ postgres/01_create_tables.sql
@@ -1,14 +1,15 @@
 
-
 \c worduel_db
-
 
 -- two different languages can have words that are spelled the same, so we use an autoincremented id
 -- we could use the word-language pair as primary key, but it doesn't feel like a good solution
 CREATE TABLE IF NOT EXISTS words(
     word_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     word    char(8) NOT NULL,
-    lang    char(2) NOT NULL
+    lang    char(2) NOT NULL,
+
+    -- ensure no duplicate words in the same language
+    CONSTRAINT unique_word_lang UNIQUE (word, lang)
 );
 
 CREATE TABLE IF NOT EXISTS word_stats(
@@ -18,5 +19,3 @@ CREATE TABLE IF NOT EXISTS word_stats(
     won_game_count INTEGER NOT NULL DEFAULT 0 CHECK (won_game_count>=0),
     guess_count    INTEGER NOT NULL DEFAULT 0 CHECK (guess_count>=0)
 );
-
-
