b40759bd60408c4b11986c2711fed7c6173658e7
Wojciech Orański <wojtekor9@gmail.com>
2026-01-23 00:20:18 +0100

    Use coockies for authentication

--- backend/src/AuthenticationService.ts
+++ backend/src/AuthenticationService.ts
@@ -12,14 +12,23 @@ export interface LoginInput {
 }
 
 export interface LoginLocals {
-  user_id: number;
-  username: string;
+  logged_in_user: null | {
+    user_id: number;
+    username: string;
+  }
+}
+
+export interface LoginLocalsEnsured {
+  logged_in_user: {
+    user_id: number;
+    username: string;
+  }
 }
 
 export async function getUser(
   req: Request<{}, {}, LoginInput>,
-  res: Response<any, LoginLocals>,
-  next: NextFunction) {
+  res: Response<any, LoginLocalsEnsured>,
+  next: NextFunction){
   const { email, password } = req.body;
 
   if (password === undefined || password.length == 0) {
@@ -37,7 +46,7 @@ export async function getUser(
     // Check both 'atomically'. Do not differentiate between wrong password and non-existent user.
     const canLogin = data && await bcrypt.compare(password, data.password_hash);
     if (canLogin) {
-      res.locals = {
+      res.locals.logged_in_user = {
         user_id: data.user_id,
         username: data.username
       };
@@ -65,17 +74,24 @@ interface UserPayload extends JwtPayload {
   username: string;
 }
 
-export function isAuthenticated(
+
+
+/*
+if session cookie is present then store logged-in user data in res.local.logged_in_user
+Otherwise set it to null.
+*/
+export function readSessionCookies(
   req: Request,
   res: Response<any, LoginLocals>,
   next: NextFunction) {
-  const authHeader = req.headers.authorization;
-  // Apparently this 'Bearer ' thingy is a standard, crazy...
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: "Missing or invalid Authorization header" }).end();
+  // we are using unsigned cookies, because jwt is already secure
+  const sessionCookie = req.cookies.worduelSessionCookie  as string; 
+  if (!sessionCookie){
+    res.locals.logged_in_user = null;
+    return next();
   }
-
-  const token = authHeader.split(' ')[1];
+  
+  const token = sessionCookie;
 
   jwt.verify(token, jwtSecret, (err, decoded) => {
     if (err) {
@@ -84,8 +100,10 @@ export function isAuthenticated(
     
     if (decoded && typeof decoded !== "string" && (decoded as UserPayload).user_id) {
       const payload = decoded as UserPayload;
-      res.locals.user_id = payload.user_id;
-      res.locals.username = payload.username;
+      res.locals.logged_in_user = {
+        user_id: payload.user_id,
+        username: payload.username,
+      };
       return next();
     } else {
       return res.status(403).json({ message: "Token does not contain user data" }).end();
